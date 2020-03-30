6284df13a2e47e8cd5c91962f887d321075db6b1
kodlekereanup <anup.kodlekere@gmail.com>
2020-03-30 21:40:39 +0530

    minimal error handling

--- ExpressionParser/tokenizer.cpp
+++ ExpressionParser/tokenizer.cpp
@@ -6,9 +6,24 @@
 // Add File Support: Read the file and tokenize it.
 // Add Exception Handling
 
-std::vector<std::string> tokenize(const std::string& line, const char& delim) {
+// enum Token_Type {
+
+// 		 TOKEN_IDENT,
+// 		 TOKEN_KEY,
+// 		 TOKEN_NUMBER,
+// 		 TOKEN_STRING
+// };
+
+// Token_Type *peek_next_token();
+
+std::vector<std::string> tokenize(const std::string& line, const char delim = ' ') {
   std::vector<std::string> tokens(line.size()); 
   int index = 0;
+
+  if(line == "") {
+    std::cout << "\n String cannot be empty! \n";
+    exit(1);
+  }
 
   for(char c : line) {
     if(c == delim) index++;
@@ -21,10 +36,28 @@ std::vector<std::string> tokenize(const std::string& line, const char& delim) {
 int main() {
 
   std::string str = "Anup is a programmer";
+  std::string t = "";
   char delim = ' ';
-
-  std::vector<std::string> tokens = tokenize(str, delim);
+  
+  std::vector<std::string> tokens = tokenize(t, delim);
   for(std::string token : tokens) std::cout << token << "\n";
 
   return 0;
 }
+
+// What I want to do:
+
+// Given a file as input,
+// Break the file into tokens
+// Ex:
+//
+// File:
+// Anup + Ankit = Awesome
+//
+// Tokens: Anup     : string
+//         Ankit    : string
+//         Awesome  : string
+//         +        : expr
+//         space    : expr (?)
+//         =        : expr
+    
